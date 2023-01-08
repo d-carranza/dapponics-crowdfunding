016ecbdaa46bbd053d370c9664eb3c2a5f737051
d-carranza <diego.carranza@protonmail.com>
2023-01-09 06:38:41 +0900

    Contract deployed

---
+++ ethereum/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "Change the seed phrase",
+  "Change the API endpoint, such as infura"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
+
+// Contract deployed to 0x7157Ec96B1d583Cfc3C34eA1f2DB0013DcB73D7c
